9dd25960798145ef8a7d9597fff71e6de5510c0b
Michal Salanci <michalsalanci@Michals-MacBook-Air.local>
2026-05-09 13:33:51 +0200

    hardcoding versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,12 +6,15 @@
 # aws provider >= 6.43 to support episodic memory
 
 terraform {
-  required_version = ">= 1.14.0"
-
+  required_version = ">= 1.14.0" 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.43"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.12"
     }
   }
 }
